7e1c026afa3fb6010bc7b0cef667f2aa53e9f23b
benatespina <benatespina@gmail.com>
2015-08-03 14:13:53 +0200

    More minor improvements

--- web/app.php
+++ web/app.php
@@ -1,9 +1,21 @@
 <?php
 
+/*
+ * This file is part of the Symfony Standard project.
+ *
+ * Copyright (c) 2015 LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Jon Torrado <jontorrado@gmail.com>
+ * @author Beñat Espiña <benatespina@gmail.com>
+ */
+
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
 
 // Enable APC for autoloading to improve performance.
 // You should change the ApcClassLoader first argument to a unique prefix
@@ -15,7 +27,7 @@ $loader->unregister();
 $apcLoader->register(true);
 */
 
-require_once __DIR__.'/../app/AppKernel.php';
+require_once __DIR__ . '/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
