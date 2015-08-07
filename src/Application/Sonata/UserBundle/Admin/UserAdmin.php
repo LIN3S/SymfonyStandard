55cdb9b578a93d255debfd3cc9ee4ecaa16e902e
Jon T. <jtorrado@lin3s.com>
2015-08-07 11:08:27 +0200

    UserAdmin comments

--- src/Application/Sonata/UserBundle/Admin/UserAdmin.php
+++ src/Application/Sonata/UserBundle/Admin/UserAdmin.php
@@ -1,19 +1,34 @@
 <?php
 
+/*
+ * This file is part of the Symfony Standard project.
+ *
+ * Copyright (c) 2015 LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Application\Sonata\UserBundle\Admin;
 
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\UserBundle\Admin\Entity\UserAdmin as BaseUserAdmin;
 
-class UserAdmin extends BaseUserAdmin {
-
-    protected function configureFormFields(FormMapper $formMapper) {
+/**
+ * Modifies Sonata Admin default User admin screens.
+ *
+ * @author Jon Torrado <jontorrado@gmail.com>
+ */
+class UserAdmin extends BaseUserAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
         parent::configureFormFields($formMapper);
     }
 
-    protected function configureListFields(ListMapper $listMapper) {
+    protected function configureListFields(ListMapper $listMapper)
+    {
         parent::configureListFields($listMapper);
     }
-
 }
