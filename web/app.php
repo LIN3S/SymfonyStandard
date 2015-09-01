bdc94489cadad0a5cc5f46b0a3b1356775022b22
Jon T <jtorrado@lin3s.com>
2015-09-01 09:17:11 +0200

    Modified commented code

--- web/app.php
+++ web/app.php
@@ -22,19 +22,14 @@ $loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
 // in order to prevent cache key conflicts with other applications
 // also using APC.
 
-//$apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
-//$loader->unregister();
-//$apcLoader->register(true);
+// See: https://github.com/symfony/symfony-standard/blob/2.7/web/app.php
 
 require_once __DIR__ . '/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
+// See: https://github.com/symfony/symfony-standard/blob/2.7/web/app.php
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
