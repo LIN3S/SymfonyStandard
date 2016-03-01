3bb163e93bddb12340c04c57a9b601f8d0679753
Jon T <jtorrado@lin3s.com>
2016-03-01 08:45:18 +0100

    Added PostCSS and updated gulp task and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -13,10 +13,12 @@
 'use strict';
 
 var gulp = require('gulp'),
-  autoprefixer = require('gulp-autoprefixer'),
   concat = require('gulp-concat'),
+  cssnext = require('postcss-cssnext'),
   livereload = require('gulp-livereload'),
   cssnano = require('gulp-cssnano'),
+  plumber = require('gulp-plumber'),
+  postcss = require('gulp-postcss'),
   rename = require('gulp-rename'),
   uglify = require('gulp-uglify'),
   sass = require('gulp-sass'),
@@ -30,48 +32,72 @@ var paths = {
   vendor: './app/Resources/assets/vendor'
 };
 
+// Plumber error function
+function onError(err) {
+  console.log(err);
+  this.emit('end');
+}
+
 gulp.task('sass', ['scsslint'], function () {
   gulp.src(paths.sass + '/app.scss')
-    .pipe(sass({
-      errLogToConsole: true
+    .pipe(plumber({
+      errorHandler: onError
     }))
-    .pipe(autoprefixer())
+    .pipe(sass())
+    .pipe(postcss([cssnext]))
     .pipe(gulp.dest(paths.css))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(livereload())
+  ;
+});
+
+gulp.task('sass:prod', function () {
+  gulp.src(paths.sass + '/app.scss')
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(sass())
+    .pipe(postcss([cssnext]))
     .pipe(cssnano())
-    .pipe(gulp.dest(paths.css));
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(paths.css))
+  ;
 });
 
 gulp.task('scsslint', function () {
   gulp.src([
-    paths.sass + '/**/*.scss',
-    '!' + paths.sass + '/base/_reset.scss'
-  ])
-  .pipe(scsslint({
-    'config': '.scss_lint.yml'
-  }));
+      paths.sass + '/**/*.scss',
+      '!' + paths.sass + '/base/_reset.scss'
+    ])
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(scsslint({
+      'config': '.scss_lint.yml'
+    }))
+  ;
 });
 
 gulp.task('js:prod', function () {
   gulp.src([
-    paths.vendor + '/jquery/dist/jquery.js',
-    paths.vendor + '/foundation-sites/dist/foundation.js',
-    paths.js + '/**/*.js'
-  ])
-  .pipe(concat('app.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest(paths.buildJs));
+      paths.vendor + '/jquery/dist/jquery.js',
+      paths.vendor + '/foundation-sites/dist/foundation.js',
+      paths.js + '/**/*.js'
+    ])
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(concat('app.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.buildJs))
+  ;
 });
 
 gulp.task('watch', function () {
-  var onChange = function (event) {
-    livereload.changed('');
-  };
   livereload.listen();
-  gulp.watch(paths.sass + '/**/*.scss', ['sass'])
-    .on('change', onChange);
-  gulp.watch(paths.js + '/**/*.js', ['js:prod'])
-    .on('change', onChange);
+  gulp.watch(paths.sass + '/**/*.scss', ['sass']);
+  gulp.watch(paths.js + '/**/*.js', ['js:prod']);
 });
 
-gulp.task('prod', ['sass', 'js:prod']);
+gulp.task('default', ['sass', 'js', 'watch']);
+
+gulp.task('prod', ['sass:prod', 'js:prod']);
