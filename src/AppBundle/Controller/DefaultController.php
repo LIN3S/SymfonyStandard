e3d776d94b44d523659508b82df7d3b13da24b71
Jon T <jtorrado@lin3s.com>
2016-06-07 12:52:40 +0200

    Updated default homepage

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -26,13 +26,16 @@ class DefaultController extends Controller
     /**
      * Index action.
      *
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      * @Method("GET")
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
     }
 }
